0a2341805f11f508f77493823b7958ac0ef58536
Faubricio Chaves <faubricio98@gmail.com>
2020-10-09 18:57:37 -0600

    Acceso de Datos para empleado. Métodos del CRUD

---
+++ MTL/Entidad/Empleado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Entidad
+{
+    public class Empleado
+    {
+		public int TN_Id_Usuario { set; get; }
+		public string TC_Usuario { set; get; }
+		public string TC_Contrasena { set; get; }
+		public string TC_Identificacion { set; get; }
+		public string TC_Nombre_Usuario { set; get; }
+		public string TC_Primer_Apellido { set; get; }
+		public string TC_Segundo_Apellido { set; get; }
+		public string TC_Tipo_Usuario { set; get; }
+		public string TC_Correo { set; get; }
+		public int TB_Activo { set; get; }
+		public int TN_Id_Puesto { set; get; }
+		public int TN_Id_Oficina { set; get; }
+		public int TB_Eliminado { set; get; }
+	}
+}
