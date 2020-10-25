1ee578e928f07b9d9ce65b7a50c49d500697b8e4
Randall Centeno <randallgears1998@gmail.com>
2020-10-24 23:35:18 -0600

    Avance 24/10

---
+++ MTL/Entidades/Oficina.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    public class Oficina
+    {
+        public int TN_Id_Oficina { set; get; }
+        public string TC_Nombre_Oficina { set; get; }
+    }
+}
