802b859d151da8d732a30b66352ddd478d5c5bf8
Daniela <danimontero155@gmail.com>
2020-11-12 18:37:30 -0600

    no tocar

--- Manejo_tiempos_laborales/Presentacion/Scripts/js/scriptsTables.js
+++ Manejo_tiempos_laborales/Presentacion/Scripts/js/scriptsTables.js
@@ -1,33 +1,13 @@
 ﻿
-var listado = []; // Aqui los datos recuperados de DB
-var listadoFiltro = []; // Datos filtardos de la pagina( inician con el listado completo)
+let TablaOriginal = ""; // Aqui los datos recuperados de DB
+let contenidoOriginal =""
+let TablaFiltro = ""; // Datos filtardos de la pagina( inician con el listado completo)
 var listadoBuscar = []; // Datos del buscador, buscan en la listaFiltro
+
 
 $(document).ready(function () {
 
-     $('#table').DataTable({
-        "searching": false,
-        "paging": true,
-        "info": false,
-        "lengthChange": false,
-        "responsive": true,
-        "autoWidth": false,
-
-
-    });
-
-
-});
-
-
-$("#searchInput").on("keyup", function () {
- 
-    filter();
-});
-
-
-function reloadDatatable() {
-     $('#table').DataTable({
+    var tabla = $('#table').DataTable({
         "searching": false,
         "paging": true,
         "info": false,
@@ -35,8 +15,11 @@ function reloadDatatable() {
         "responsive": true,
         "autoWidth": false,
     });
-}
 
+    TablaOriginal = tabla;
+    contenidoOriginal = document.getElementById("contenidoTabla");
+
+});
 
 
 
@@ -51,6 +34,30 @@ function reloadDatatable() {
 
 
 
+$("#searchInput").on("keyup", function () {
+
+    filter();
+});
+
+
+
+
+function reloadDatatable() {
+    $('#table').DataTable().destroy();
+    $('#table').DataTable({
+        "searching": false,
+        "paging": true,
+        "info": false,
+        "lengthChange": false,
+        "responsive": true,
+        "autoWidth": false,
+    });
+
+    document.getElementById("contenidoTabla").value = contenidoOriginal;
+}
+
+
+
 function filter() {
     var value = $("#searchInput").val().toLowerCase();
     $("#contenidoTabla tr").filter(function () {
@@ -59,83 +66,85 @@ function filter() {
 }
 
 
+function separarFechas(rango) {
+    var separador = rango.indexOf('-');
+    var fechaI = rango.substring(0, separador - 1)
+    var fechaF = rango.substring(separador + 2, rango.length);
+    return [fechaI, fechaF];
+}
 
 
 
 function filtrarAusencias() {
 
-    $("#contenidoTabla tr").filter(function () {
-        var motivoValue = document.getElementById("motivoFiltro");
-        valueMotivo = motivoValue.options[motivoValue.selectedIndex].text.toLowerCase();
-        $(this).toggle($(this).text().toLowerCase().indexOf(valueMotivo) > -1)
-    })
 
-
-    /*
+     var motivoValue = document.getElementById("motivoFiltro");
      var fechaFiltro = document.getElementById("fechaFomatFiltro").value;
      let rows = document.querySelectorAll("#contenidoTabla tr");  // Obtener las filas de la tabla
 
-    
+
     for (let this_row = 0; this_row < rows.length; this_row++) {
-        if (motivoFiltro.length > 0) { // Si se filtra por motivo
+        var row = $(rows[this_row]).closest('tr');                  // Fila actual
+        var filtrar = false;
+        /*if ((motivoFiltro.length > 0) || (fechaFiltro.length > 0) ) { // Si se filtra por motivo
 
             var motivo = motivoValue.options[motivoValue.selectedIndex].text;
-            var row = $(rows[this_row]).closest('tr');
+            
             var tipo = row.find("td").eq(4).html();            
 
-            if (tipo != motivo ) {  // Si el tipo de la tabla es igual al del filtro entonces no ocul
-                //rows[this_row].style.visibility = "hidden"; // Ocultar elemento
-                rows[this_row].hide();
+            if (tipo != motivo ) {  // Si el tipo de la tabla es igual al del filtro entonces remover
+                rows[this_row].remove();
                 
             }
-        }
+        } */
 
         if (fechaFiltro.length > 0) {//A,B,D
-            var fechaSalida = separarFechasHistoricoAusencia(fechaValue)[0];
-            var fechaRegreso = separarFechasHistoricoAusencia(fechaValue)[1];
+            var fechaSalida = separarFechas(fechaFiltro)[0];
+            var fechaRegreso = separarFechas(fechaFiltro)[1];
+            var dateSalidaFiltro = new Date(fechaSalida);
+            var dateRegresoFiltro = new Date(fechaRegreso);
 
-            document.getElementById("fechaSalida").style.visibility = "hidden";
-            document.getElementById("fechaRegreso").style.visibility = "hidden";
+            var dateSalidaTabla = new Date(row.find("td").eq(5).html());
+            var dateRegresoTabla = new Date(row.find("td").eq(6).html());
+
+            if ((dateSalidaFiltro > dateRegresoFiltro) || (dateSalidaFiltro < dateSalidaTabla) || (dateRegresoFiltro > dateRegresoTabla)) {
+                rows[this_row].remove();
+            }
         }
     }
-
-    $('#table').DataTable().destroy();
-
-    //$("#contenidoTabla tr").filter(function () {
-    //    $(this).toggle($(this).text().toLowerCase().indexOf(getComputedStyle().visibility === "hidden") > -1)
-   // })
- 
-
-    /*
-
-    $("#contenidoTabla tr").filter(function () {
-        $(this).toggle($(this).text().toLowerCase().indexOf(valueMotivo) > -1)
-    })
-    */
 }
 
 
 $('#fechaFomatFiltro').daterangepicker();
 
 $('#fechaFomatFiltro').daterangepicker({
-    locale: {
-        format: 'YYYY/MM/DD'
-    }
+    format: 'dd/mm/yyyy',
 });
+
+
 $('#fechaFomatFiltro').val('');
 $('#fechaFomatFiltro').attr("placeholder", "Rango de Fechas");
+
+
+$("#fechaFomatFiltro").click(function () {
+
+    if ($('#fechaFomatFiltro').val == '') {
+        $('#fechaFomatFiltro').daterangepicker();
+
+        $('#fechaFomatFiltro').daterangepicker("setDate", 'now');
+    }
+});
 
 
 
 
 function vaciarFiltroAusencias() {
-    $("#contenidoTabla tr").filter(function () {
-        $(this).toggle($(this).text().toLowerCase().indexOf("") > -1)
-    })
-    $("#motivoFiltro").val(0);
 
+    reloadDatatable(); 
+    $("#motivoFiltro").val(0);
     vaciarFecha();
 }
+
 
 
 function vaciarFecha() {
